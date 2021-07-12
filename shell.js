d2f6bee0b34a0655db07e9ead206de48e9ca8d98
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-07-12 20:59:27 +0200

    update shell file

--- shell.js
+++ shell.js
@@ -1,30 +1,43 @@
 const shell = require("shelljs");
 
-//NOTE range can be --caret or --tilde
-const upgrade = (callback, spinner, range = "") =>
-  shell.exec(
-    `yarn --cwd ${dir} upgrade ${range}`,
-    {
-      silent: true
-    },
-    (_, stdout) => callback(stdout, spinner)
-  );
-
 const test = (option, value) => shell.test(option, value);
 
-const audit = (callback, spinner, target = process.cwd()) =>
+const audit = (spinner, hint, target, callback, teardown) => {
+  spinner.text = "scanning for vulnerabilities" + hint;
+
   shell.exec(
     `yarn --cwd ${target} audit --json`,
     {
       silent: true
     },
-    (_, stdout) => callback(stdout, spinner)
+    (_, stdout) => callback(stdout, spinner, hint, target, teardown)
   );
+};
 
-const install = () =>
-  shell.exec(`yarn --cwd ${dir} install`, {
-    silent: true
-  });
+//TODO: add range that can be --caret or --tilde
+const upgrade = (spinner, hint, target, callback, range = "") => {
+  spinner.text = "upgrading packages" + hint;
+
+  shell.exec(
+    `yarn --cwd ${target} upgrade ${range}`,
+    {
+      silent: true
+    },
+    (_, stdout) => callback(stdout, spinner, hint, target)
+  );
+};
+
+const install = (spinner, hint, target) => {
+  spinner.text = "install packages" + hint;
+
+  shell.exec(
+    `yarn --cwd ${target} install`,
+    {
+      silent: true
+    },
+    () => spinner.succeed("installed successfully")
+  );
+};
 
 module.exports = {
   test,

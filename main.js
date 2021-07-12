51be418296837b6996dda3c1181d26b228cf6be2
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-07-12 21:00:09 +0200

    refactor main file to simplify entry logic

--- main.js
+++ main.js
@@ -2,72 +2,22 @@
 
 require("colors");
 const ora = require("ora");
-const { test } = require("./shell");
-const { audit } = require("./audit");
-const { resolve } = require("./resolve");
 
-const Flags = {
-  fix: ["--fix", "-f"],
-  path: ["--path", "-p"],
-  audit: ["--audit", "-a"],
-  revert: ["--revert", "-r"],
-  upgrade: ["--upgrade", "-u"],
-  cleanup: ["--cleanup", "-c"],
-  install: ["--install", "-i"]
-};
+const { controller } = require("./controller");
 
 const spinner = ora("starting service").start();
 
 const [, , ...inputs] = process.argv;
 
-let error;
-let index;
-let target;
-let cleanup;
-let install;
-let service;
+const { service, callback, target, preparatory, teardown, error } = controller(
+  spinner,
+  inputs
+);
 
-inputs.forEach((input, i) => {
-  if (!target && index !== i) {
-    if (Flags.path.includes(input)) {
-      index = i + 1;
-      const path = inputs[index];
+const hint = target !== process.cwd() ? ` in (${target})`.gray : "";
 
-      if (!test("-e", path)) {
-        error = true;
-        return spinner.fail("invalid path provided");
-      }
-
-      target = path;
-    }
-  }
-
-  if (!cleanup) {
-    if (Flags.cleanup.includes(input)) {
-      cleanup = true;
-    }
-  }
-
-  if (!install) {
-    if (Flags.install.includes(input)) {
-      install = true;
-    }
-  }
-
-  if (!service) {
-    if (Flags.audit.includes(input)) {
-      service = audit;
-    } else if (Flags.fix.includes(input)) {
-      service = resolve;
-    } else if (Flags.upgrade.includes(input)) {
-      //callback = upgrade;
-    }
-  }
-});
-
-if (error) return;
-
-const hint = target ? ` in (${target})`.gray : "";
-spinner.text = "scanning package.json file" + hint;
-
-if (service) service(spinner, target || process.cwd(), cleanup, install);
+if (error) spinner.fail(error);
+else if (preparatory)
+  preparatory(service, spinner, hint, target, callback, teardown);
+else if (service) service(spinner, hint, target, callback, teardown);
+else spinner.fail("invalid command");

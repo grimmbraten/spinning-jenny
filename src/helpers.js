f709f45a9d3f43633a3873a80aa898c60cc03f73
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-07-16 14:22:44 +0200

    add additional helpers

--- src/helpers.js
+++ src/helpers.js
@@ -4,6 +4,13 @@ const parseJson = json =>
     .map(step => (step ? JSON.parse(step) : undefined))
     .filter(data => data);
 
+const scannedDependencies = json =>
+  json.filter(data => data.type === "auditSummary")[0].data.totalDependencies;
+
+const resolutionCount = resolutions => Object.entries(resolutions).length;
+
 module.exports = {
-  parseJson
+  parseJson,
+  resolutionCount,
+  scannedDependencies
 };

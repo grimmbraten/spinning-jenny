803e847e31dc9f7b28474ffaf2b216d7d84efe99
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-07-16 13:43:32 +0200

    cleanup main and relocate hint decleration

--- src/main.js
+++ src/main.js
@@ -5,27 +5,27 @@ const ora = require("ora");
 
 const { controller } = require("./controller");
 
-const spinner = ora();
-
 let promises = [];
 const [, , ...inputs] = process.argv;
 
 (async () => {
-  const { preparatory, compiler, teardown, target, handler, error } =
+  const spinner = ora();
+
+  const { preparatory, compiler, teardown, target, handler, error, hint } =
     controller(inputs);
 
   if (error) return spinner.fail(error);
-
-  const hint = target !== process.cwd() ? ` in (${target})`.gray : "";
 
   preparatory.forEach(async action => {
     promises.push(action(spinner, hint, target));
   });
 
   await Promise.all(promises);
-
   promises = [];
+
   const response = await compiler(spinner, hint, target);
+
+  if (!response) return spinner.fail("something went wrong, sorry about that");
 
   teardown.forEach(action => {
     promises.push(action(spinner, hint, target));
@@ -33,5 +33,5 @@ const [, , ...inputs] = process.argv;
 
   await Promise.all(promises);
 
-  if (handler) handler(response, spinner, hint, target);
+  handler && handler(response, spinner, hint, target);
 })();

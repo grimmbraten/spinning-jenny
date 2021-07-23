c66852556288eed339f9d860f72b202f21d707ea
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-07-23 13:42:10 +0200

    add variable color to config set output

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 require("colors");
 
-const { read, write, isBooleanInput } = require("./helpers");
+const { read, write, isBooleanInput, colorVariable } = require("./helpers");
 
 const Flags = {
   label: ["--label", "label", "-l"],
@@ -36,42 +36,37 @@ const editConfig = async (spinner, inputs) => {
 
   inputs.forEach((input, index) => {
     if (index % 2 === 1) return;
+    spinner.start("updating configuration");
 
     if (Flags.verbose.includes(input)) {
-      spinner.start("changing verbose mode");
-
       const value = isBooleanInput(inputs[index + 1]);
       if (value === undefined)
         return spinner.fail("verbose can only be set to true / false");
 
       config.verbose = value;
-      spinner.info("changed verbose to " + value);
+      spinner.info("set verbose to " + colorVariable(value));
     } else if (Flags.frozen.includes(input)) {
-      spinner.start("changing frozen mode");
-
       const value = isBooleanInput(inputs[index + 1]);
       if (value === undefined)
         return spinner.fail("frozen can only be set to true / false");
 
       config.frozen = value;
-      spinner.info("changed frozen to " + value);
+      spinner.info("set frozen to " + colorVariable(value));
     } else if (Flags.backup.includes(input)) {
       const value = isBooleanInput(inputs[index + 1]);
       if (value === undefined)
         return spinner.fail("frozen can only be set to true / false");
 
       config.backup = value;
-      spinner.info("changed backup to " + value);
+      spinner.info("set backup to " + colorVariable(value));
     } else if (Flags.label.includes(input)) {
       const value = isBooleanInput(inputs[index + 1]);
       if (value === undefined)
         return spinner.fail("label can only be set to true / false");
 
       config.label = value;
-      spinner.info("changed label to " + value);
+      spinner.info("set label to " + colorVariable(value));
     } else if (Flags.pattern.includes(input)) {
-      spinner.start("changing upgrade pattern");
-
       let value = inputs[index + 1];
       if (!value) return spinner.fail("please pass a value");
 
@@ -84,7 +79,7 @@ const editConfig = async (spinner, inputs) => {
         );
 
       config.pattern = value;
-      spinner.info("changed pattern to ", value);
+      spinner.info("set pattern to ", colorVariable(value));
     }
   });
 

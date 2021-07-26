14b9c64525ef3f9a4031b6beff27b78c54e9cb61
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-07-26 14:07:05 +0200

    add catch scopes and improve output messages

--- src/compilers.js
+++ src/compilers.js
@@ -4,183 +4,222 @@ const {
   read,
   write,
   remove,
+  loader,
   parseJson,
+  stepLabel,
   resolutionCount,
   scannedDependencies,
   extractUpgradeOutcome
 } = require("./helpers");
 
-const dry = (spinner, hint, target, { label, verbose, ...config }) =>
-  new Promise(async function (resolve, reject) {
-    const step = config.getStep();
-    label && config.steps.completed++;
+const test = (option, value) => shell.test(option, value);
 
-    verbose && spinner.start(step + "removing resolutions" + hint);
+const dry = (spinner, hint, target, { verbose, ...config }) =>
+  new Promise(async function (resolve, reject) {
+    const step = stepLabel(config);
+    loader(verbose, spinner, "start", "removing resolutions", step, hint);
 
     const resolutions = await read(target, "package.json", "resolutions");
 
     if (!resolutions) {
-      verbose &&
-        spinner.fail(
-          step + "package.json does not include any resolutions" + hint
-        );
-      return resolve();
-    }
-
-    const success = await remove(target, "package.json", "resolutions");
-
-    if (success) {
-      verbose &&
-        spinner.succeed(
-          step + `removed ${resolutionCount(resolutions)} resolutions` + hint
-        );
-      resolve();
+      loader(
+        verbose,
+        spinner,
+        "warn",
+        "could not find any resolutions to remove",
+        step,
+        hint
+      );
     } else {
-      verbose && spinner.fail(step + "remove resolutions failed" + hint);
-      reject();
+      const success = await remove(target, "package.json", "resolutions");
+
+      if (success) {
+        loader(
+          verbose,
+          spinner,
+          "succeed",
+          `removed ${resolutionCount(resolutions)} resolution(s)`,
+          step,
+          hint
+        );
+      } else return reject();
     }
-  });
 
-const test = (option, value) => shell.test(option, value);
+    resolve();
+  }).catch(() =>
+    loader(verbose, spinner, "fail", "failed to remove resolutions", step, hint)
+  );
 
-const audit = (spinner, hint, target, { label, verbose, ...config }) => {
-  const step = config.getStep();
-  label && config.steps.completed++;
+const audit = (spinner, hint, target, { verbose, ...config }) =>
+  new Promise(function (resolve, reject) {
+    const step = stepLabel(config);
+    loader(
+      verbose,
+      spinner,
+      "start",
+      "scanning for vulnerabilities",
+      step,
+      hint
+    );
 
-  verbose && spinner.start(step + "scanning for vulnerabilities" + hint);
-
-  return new Promise(function (resolve, reject) {
     shell.exec(
       `yarn --cwd ${target} audit --json`,
       {
         silent: true
       },
       (_, stdout, stderr) => {
-        if (stderr) {
-          verbose && spinner.fail(step + "scan failed" + hint);
-          reject(stderr);
-        }
+        if (stderr) return reject(stderr);
 
-        verbose &&
-          spinner.succeed(
-            step +
-              `scanned ${scannedDependencies(parseJson(stdout))} dependencies` +
-              hint
-          );
-        return resolve(stdout);
+        loader(
+          verbose,
+          spinner,
+          "succeed",
+          `scanned ${scannedDependencies(parseJson(stdout))} dependencies`,
+          step,
+          hint
+        );
+
+        resolve(stdout);
       }
     );
-  });
-};
+  }).catch(() =>
+    loader(
+      verbose,
+      spinner,
+      "fail",
+      "failed to scan for vulnerabilities",
+      step,
+      hint
+    )
+  );
 
-const upgrade = (
-  spinner,
-  hint,
-  target,
-  { label, verbose, pattern, ...config }
-) => {
-  const step = config.getStep();
-  label && config.steps.completed++;
+const upgrade = (spinner, hint, target, { verbose, pattern, ...config }) =>
+  new Promise(function (resolve, reject) {
+    const step = stepLabel(config);
+    loader(verbose, spinner, "start", "upgrading packages", step, hint);
 
-  verbose && spinner.start(step + "upgrading packages" + hint);
-
-  return new Promise(function (resolve, reject) {
     shell.exec(
       `yarn --cwd ${target} upgrade ${pattern} --json`,
       {
         silent: true
       },
       (_, stdout, stderr) => {
-        if (stderr) {
-          verbose && spinner.fail(step + "upgrade failed" + hint);
-          reject(stderr);
-        }
+        if (stderr) return reject(stderr);
 
         const outcome = extractUpgradeOutcome(parseJson(stdout));
 
-        if (!outcome) {
-          verbose && spinner.fail(step + "upgrade failed");
-          return resolve(stdout);
-        }
+        if (!outcome) return reject();
 
-        verbose && spinner.succeed(step + outcome + hint);
-        return resolve(stdout);
+        loader(verbose, spinner, "succeed", outcome, step, hint);
+        resolve(stdout);
       }
     );
-  });
-};
+  }).catch(() =>
+    loader(verbose, spinner, "fail", "failed to upgrade packages", step, hint)
+  );
 
-const install = (spinner, hint, target, { label, verbose, ...config }) => {
-  const step = config.getStep();
-  label && config.steps.completed++;
+const install = (spinner, hint, target, { verbose, ...config }) =>
+  new Promise(function (resolve, reject) {
+    const step = stepLabel(config);
+    loader(verbose, spinner, "start", "installing packages", step, hint);
 
-  verbose && spinner.start(step + "install packages" + hint);
-
-  return new Promise(function (resolve, reject) {
     shell.exec(
       `yarn --cwd ${target} install`,
       {
         silent: true
       },
       (_, stdout, stderr) => {
-        if (stderr) {
-          verbose && spinner.fail(step + "installation failed" + hint);
-          reject(stderr);
-        }
+        if (stderr) return reject(stderr);
 
-        verbose && spinner.succeed(step + "installed successfully" + hint);
-        return resolve(stdout);
+        loader(
+          verbose,
+          spinner,
+          "succeed",
+          "successfully installed packages",
+          step,
+          hint
+        );
+
+        resolve(stdout);
       }
     );
-  });
-};
+  }).catch(() =>
+    loader(verbose, spinner, "fail", "failed to installed packages", step, hint)
+  );
 
-const backup = async (spinner, hint, target, { label, verbose, ...config }) => {
+const backup = async (spinner, hint, target, { verbose, ...config }) => {
   let backup = {};
-  const step = config.getStep();
-  label && config.steps.completed++;
+  const step = stepLabel(config);
+
+  loader(
+    verbose,
+    spinner,
+    "start",
+    "scanning for existing resolutions",
+    step,
+    hint
+  );
 
   const resolutions = await read(target, "package.json", "resolutions");
-  if (!resolutions) {
-    verbose &&
-      spinner.info(step + "could not find any resolutions to backup" + hint);
-    return;
-  }
 
-  verbose && spinner.start(step + "backing up resolutions" + hint);
+  if (!resolutions)
+    return loader(
+      verbose,
+      spinner,
+      "info",
+      "could not find any resolutions to backup",
+      step,
+      hint
+    );
 
   const dir = target.split("/").pop();
-
   backup[dir] = { resolutions, date: new Date().toString() };
-
   await write("./src", ".backups.json", backup);
 
-  verbose && spinner.succeed(step + "created backup of resolutions" + hint);
+  loader(
+    verbose,
+    spinner,
+    "succeed",
+    "successfully created backup of resolutions",
+    step,
+    hint
+  );
 };
 
-const original = async (
-  spinner,
-  hint,
-  target,
-  { label, verbose, ...config }
-) => {
-  const step = config.getStep();
-  label && config.steps.completed++;
-
-  verbose && spinner.start(step + "applying original resolutions" + hint);
+const original = async (spinner, hint, target, { verbose, ...config }) => {
+  const step = stepLabel(config);
+  loader(
+    verbose,
+    spinner,
+    "start",
+    "applying original resolutions",
+    step,
+    hint
+  );
 
   const dir = target.split("/").pop();
-
   const { resolutions } = await read("./src", ".backups.json", dir);
 
-  if (!resolutions) {
-    verbose && spinner.fail(step + "found no resolution backup" + hint);
-    return;
-  }
+  if (!resolutions)
+    return loader(
+      verbose,
+      spinner,
+      "fail",
+      "failed to apply original resolutions",
+      step,
+      hint
+    );
 
   await write(target, "package.json", { resolutions });
 
-  verbose && spinner.succeed(step + "applied original resolutions" + hint);
+  loader(
+    verbose,
+    spinner,
+    "succeed",
+    "successfully applied original resolutions",
+    step,
+    hint
+  );
 };
 
 module.exports = {
@@ -188,7 +227,7 @@ module.exports = {
   test,
   audit,
   backup,
-  original,
+  install,
   upgrade,
-  install
+  original
 };

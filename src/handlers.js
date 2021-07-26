fbdd2f371941602cda9e42fbdfc43f944ad0ff50
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-07-26 11:25:12 +0200

    add patches handler

--- src/handlers.js
+++ src/handlers.js
@@ -4,6 +4,7 @@ const {
   read,
   write,
   parseJson,
+  severityBadge,
   colorVariable,
   extractAuditSummary
 } = require("./helpers");
@@ -57,22 +58,23 @@ const report = (response, spinner, hint, target, { verbose }) => {
     vulnerabilities: { critical, high, moderate, low, info }
   } = data;
 
-  const criticalBadge = critical
-    ? chalk.bgMagenta.white(`  ${critical} critical  `)
-    : "";
-  const highBadge = high ? " " + chalk.bgRed.white(`  ${high} high  `) : "";
+  const criticalBadge = critical ? severityBadge("critical", critical) : "";
+  const highBadge = high ? " " + severityBadge("high", high) : "";
   const moderateBadge = moderate
-    ? " " + chalk.bgYellow.black(`  ${moderate} moderate  `)
+    ? " " + severityBadge("moderate", moderate)
     : "";
-  const lowBadge = low ? " " + chalk.bgGreen.black(`  ${low} low  `) : "";
-  const infoBadge = info ? " " + chalk.bgBlue.white(`  ${info} info  `) : "";
+  const lowBadge = low ? " " + severityBadge("low", low) : "";
+  const infoBadge = info ? " " + severityBadge("info", info) : "";
 
-  verbose &&
-    spinner.warn(
-      `found ${vulnerabilities} vulnerabilities` +
-        hint +
-        `\n\n${criticalBadge}${highBadge}${moderateBadge}${lowBadge}${infoBadge}`
-    );
+  verbose
+    ? spinner.warn(
+        `found ${vulnerabilities} vulnerabilities` +
+          hint +
+          `\n\n${criticalBadge}${highBadge}${moderateBadge}${lowBadge}${infoBadge}`
+      )
+    : console.log(
+        `\n${criticalBadge}${highBadge}${moderateBadge}${lowBadge}${infoBadge}`
+      );
 };
 
 const resolve = async (response, spinner, hint, target, { verbose }) => {
@@ -82,11 +84,13 @@ const resolve = async (response, spinner, hint, target, { verbose }) => {
   const vulnerabilities = sum(data.vulnerabilities);
 
   if (vulnerabilities === 0) {
-    verbose && spinner.fail("package.json contains no vulnerabilities" + hint);
+    verbose && spinner.succeed("no vulnerabilities detected" + hint);
     return;
   }
 
-  const resolutions = json
+  if (verbose) spinner.text = `resolving vulnerabilities` + hint;
+
+  let resolutions = json
     .map(({ data, type }) => {
       if (type === "auditAdvisory")
         return {
@@ -98,14 +102,13 @@ const resolve = async (response, spinner, hint, target, { verbose }) => {
           url: data.advisory.url
         };
     })
-    .filter(data => data)
-    .filter(({ patched }) => patched !== "<0.0.0");
+    .filter(data => data);
 
-  if (verbose) spinner.text = `building resolutions`;
+  const noPatch = resolutions.filter(({ patched }) => patched === "<0.0.0");
+  resolutions = resolutions.filter(({ patched }) => patched !== "<0.0.0");
 
   if (resolutions.length === 0) {
-    verbose &&
-      spinner.fail("failed to resolved package vulnerabilities" + hint);
+    verbose && spinner.fail("failed to resolved vulnerabilities" + hint);
     return;
   }
 
@@ -115,13 +118,69 @@ const resolve = async (response, spinner, hint, target, { verbose }) => {
 
   await write(target, "package.json", { resolutions: modules });
 
+  if (verbose) {
+    spinner.succeed(`successfully resolved vulnerabilities` + hint);
+    if (noPatch.length > 0)
+      spinner.warn(
+        `found ${noPatch.length} package(s) without a patch\n\n${chalk.gray(
+          `please run ${chalk.white(
+            `spinning-jenny --patches${target ? ` --directory ${target}` : ""}`
+          )} for more information`
+        )}`
+      );
+  }
+};
+
+const patches = (response, spinner, hint, target, { verbose }) => {
+  const json = parseJson(response);
+
+  if (verbose) spinner.text = `analyzing vulnerabilities` + hint;
+
+  let resolutions = json
+    .map(({ data, type }) => {
+      if (type === "auditAdvisory")
+        return {
+          title: data.advisory.title,
+          module: data.advisory.module_name,
+          version: data.advisory.vulnerable_versions,
+          patched: data.advisory.patched_versions,
+          severity: data.advisory.severity,
+          url: data.advisory.url
+        };
+    })
+    .filter(data => data);
+
+  const unique = [...new Set(resolutions.map(package => package.module))];
+
+  const patches = unique.map(module =>
+    resolutions.find(package => package.module === module)
+  );
+
+  if (patches.length === 0) {
+    verbose && spinner.fail("failed to analyze vulnerabilities" + hint);
+    return;
+  }
+
   verbose &&
-    spinner.succeed(`successfully resolved package vulnerabilities` + hint);
+    spinner.succeed(
+      `found ${patches.length} module(s) with vulnerabilities` + hint
+    );
+
+  patches.forEach(patch => {
+    console.log(
+      `\n${severityBadge(patch.severity)}\n${patch.module}${chalk.gray(
+        `@${patch.version}`
+      )}\npatched: ${
+        patch.patched !== "<0.0.0" ? chalk.green("true") : chalk.red("false")
+      }\nvulnerability: ${patch.title.toLowerCase()}\n${chalk.gray(patch.url)}`
+    );
+  });
 };
 
 module.exports = {
   report,
   resolve,
   backups,
+  patches,
   configuration
 };

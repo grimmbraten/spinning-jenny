13868611c4a43df777b2671328c7145da0707e5b
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-07-30 10:23:11 +0200

    add color error message helper

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,6 @@
-const path = require("path");
-const chalk = require("chalk");
-const json = require("json-file-plus");
+const path = require('path');
+const chalk = require('chalk');
+const json = require('json-file-plus');
 
 const sum = collection => Object.values(collection).reduce((a, b) => a + b);
 
@@ -8,48 +8,52 @@ const loader = (verbose, spinner, action, message, step, hint) => {
   if (!verbose) return;
 
   switch (action) {
-    case "start":
+    case 'start':
       spinner.start(step + message + hint);
       break;
-    case "fail":
+    case 'fail':
       spinner.fail(step + message + hint);
       break;
-    case "succeed":
+    case 'succeed':
       spinner.succeed(step + message + hint);
       break;
-    case "warn":
+    case 'warn':
       spinner.warn(step + message + hint);
       break;
-    case "text":
+    case 'text':
       spinner.text = step + message + hint;
       break;
-    case "info":
+    case 'info':
       spinner.info(step + message + hint);
   }
 };
 
 const stepLabel = ({ label, steps, getStep }) => {
-  if (!label) return "";
+  if (!label) return '';
 
   const step = getStep();
   steps.completed++;
   return step;
 };
 
-const severityBadge = (severity, count = "") => {
-  if (count !== "") count = `${count} `;
+const severityBadge = (severity, count = '') => {
+  if (count !== '') count = `${count} `;
 
-  if (severity === "critical")
-    return chalk.bgMagenta.white(`  ${count}critical  `);
-  else if (severity === "high") return chalk.bgRed.white(`  ${count}high  `);
-  else if (severity === "moderate")
-    return chalk.bgYellow.black(`  ${count}moderate  `);
-  else if (severity === "low") chalk.bgGreen.black(`  ${count}low  `);
+  if (severity === 'critical') return chalk.bgMagenta.white(`  ${count}critical  `);
+  else if (severity === 'high') return chalk.bgRed.white(`  ${count}high  `);
+  else if (severity === 'moderate') return chalk.bgYellow.black(`  ${count}moderate  `);
+  else if (severity === 'low') chalk.bgGreen.black(`  ${count}low  `);
   else return chalk.bgBlue.white(`  ${count}info  `);
 };
 
+const colorError = err =>
+  err
+    .replace(/error/g, chalk.red('error'))
+    .replace(/warning/g, chalk.yellow('warning'))
+    .replace(/Ignoring it./g, chalk.gray('Ignoring it.'));
+
 const colorVariable = value =>
-  typeof value === "string"
+  typeof value === 'string'
     ? chalk.gray(`${value}`)
     : value
     ? chalk.green(`${value}`)
@@ -61,34 +65,32 @@ const parseJson = json =>
     .map(step => (step ? JSON.parse(step) : undefined))
     .filter(data => data);
 
-const extractAuditSummary = json =>
-  json.filter(data => data.type === "auditSummary")[0];
+const extractAuditSummary = json => json.filter(data => data.type === 'auditSummary')[0];
 
 const extractUpgradeOutcome = json => {
-  const outcome = json.filter(data => data.type === "success")[1].data;
+  const outcome = json.filter(data => data.type === 'success')[1].data;
 
-  return !outcome ? undefined : outcome.replace(".", "").replace("S", "s");
+  return !outcome ? undefined : outcome.replace('.', '').replace('S', 's');
 };
 
 const scannedDependencies = json =>
-  json.filter(data => data.type === "auditSummary")[0].data.totalDependencies;
+  json.filter(data => data.type === 'auditSummary')[0].data.totalDependencies;
 
 const resolutionCount = resolutions => Object.entries(resolutions).length;
 
-const isBooleanInput = value =>
-  value === "true" ? true : value === "false" ? false : undefined;
+const isBooleanInput = value => (value === 'true' ? true : value === 'false' ? false : undefined);
 
 const read = (dir, file, property) =>
-  new Promise(async (resolve, reject) => {
-    await json(path.join(dir, file), async (err, json) => {
+  new Promise((resolve, reject) => {
+    json(path.join(dir, file), async (err, json) => {
       if (err) reject(err);
       resolve(property ? await json.get(property) : await json.get());
     });
   }).catch(err => console.error(err));
 
 const write = (dir, file, property) =>
-  new Promise(async (resolve, reject) => {
-    await json(path.join(dir, file), async (err, json) => {
+  new Promise((resolve, reject) => {
+    json(path.join(dir, file), async (err, json) => {
       if (err) reject(err);
       await json.set(property);
       await json.save();
@@ -97,8 +99,8 @@ const write = (dir, file, property) =>
   }).catch(err => console.error(err));
 
 const remove = (dir, file, property) =>
-  new Promise(async (resolve, reject) => {
-    await json(path.join(dir, file), async (err, json) => {
+  new Promise((resolve, reject) => {
+    json(path.join(dir, file), async (err, json) => {
       if (err) reject(err);
       await json.remove(property);
       await json.save();
@@ -114,6 +116,7 @@ module.exports = {
   remove,
   stepLabel,
   parseJson,
+  colorError,
   severityBadge,
   colorVariable,
   isBooleanInput,

4759040fade8c808cab4a22f346dbb4e6e315b7b
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-09-24 16:17:12 +0200

    extend function controller

--- src/controller/functions.js
+++ src/controller/functions.js
@@ -10,10 +10,11 @@ const {
   backup,
   advise,
   upgrade,
+  actions,
   flags
 } = require('../functions');
 
-const getFunctions = inputs => {
+const getFunctions = (inputs, config) => {
   let skipIndex;
   let hint = '';
   let target = process.cwd();
@@ -23,39 +24,66 @@ const getFunctions = inputs => {
   inputs.forEach((input, index) => {
     if (skipIndex === index) return;
 
-    if (flags.directory.includes(input)) {
+    if (flags.label.includes(input))
+      if (inputs[index + 1] === 'true' || inputs[index + 1] === 'false') {
+        skipIndex = index + 1;
+        config.label = inputs[skipIndex] === 'true';
+      } else config.label = true;
+    else if (flags.frozen.includes(input))
+      if (inputs[index + 1] === 'true' || inputs[index + 1] === 'false') {
+        skipIndex = index + 1;
+        config.frozen = inputs[skipIndex] === 'true';
+      } else config.frozen = true;
+    else if (flags.backup.includes(input))
+      if (inputs[index + 1] === 'true' || inputs[index + 1] === 'false') {
+        skipIndex = index + 1;
+        config.backup = inputs[skipIndex] === 'true';
+      } else config.backup = true;
+    else if (flags.verbose.includes(input))
+      if (inputs[index + 1] === 'true' || inputs[index + 1] === 'false') {
+        skipIndex = index + 1;
+        config.verbose = inputs[skipIndex] === 'true';
+      } else config.verbose = true;
+    else if (flags.directory.includes(input)) {
       skipIndex = index + 1;
       target = inputs[skipIndex];
       hint = chalk.gray(` in ${target}`);
-    } else if (flags.backup.includes(input)) {
-      if (inputs[index + 1] === 'restore') {
-        skipIndex = index++;
-        functions.push(restore);
-      } else if (inputs[index + 1] !== 'list') {
-        skipIndex = index++;
-        functions.push(backup);
-      }
-    } else if (flags.install.includes(input)) functions.push(install);
-    else if (flags.upgrade.includes(input)) functions.push(upgrade);
-    else if (flags.scan.includes(input)) functions.push(scan);
-    else if (flags.protect.includes(input)) functions.push(protect);
-    else if (flags.advisories.includes(input)) functions.push(advise);
-    else if (flags.clean.includes(input)) functions.push(clean);
-    else suggestFunction(input);
+    } else if (actions.scan.includes(input)) functions.push(scan);
+    else if (actions.clean.includes(input)) functions.push(clean);
+    else if (actions.advise.includes(input)) functions.push(advise);
+    else if (actions.restore.includes(input)) functions.push(restore);
+    else if (actions.install.includes(input)) functions.push(install);
+    else if (actions.upgrade.includes(input)) functions.push(upgrade);
+    else if (actions.protect.includes(input)) functions.push(protect);
+    else {
+      suggestFunction(inputs, input);
+      return { functions: null };
+    }
   });
 
-  return { hint, functions: functions.length > 0 ? functions : null, target };
+  config.backup && functions.unshift(backup);
+  config.steps.total = functions.length;
+
+  return { hint, functions: functions.length > 0 ? functions : null, target, config };
 };
 
-const suggestFunction = input => {
+const suggestFunction = (inputs, input) => {
   const fuzzy = new Fuse(
-    Object.values(flags)
+    Object.values({ ...flags, ...actions })
       .flat(2)
       .filter(data => data.length > 2),
     { threshold: 0.4 }
   ).search(input);
 
-  fuzzy.forEach(suggestion => console.log(suggestion.item));
+  console.log(
+    `\nsyntax error\nspinning-jenny ${inputs
+      .join()
+      .replace(input, chalk.red(input))}\n\nsuggestions:`
+  );
+
+  fuzzy.forEach(suggestion =>
+    console.log(`spinning-jenny ${inputs.join().replace(input, chalk.green(suggestion.item))}`)
+  );
 };
 
 module.exports = {

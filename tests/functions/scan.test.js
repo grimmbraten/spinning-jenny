1eb3af167388964afdf8a4ee53d8c9c5a06a5bf0
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-09-24 17:49:03 +0200

    introduce a first passing test for scan function

--- tests/functions/scan.test.js
+++ tests/functions/scan.test.js
@@ -1,7 +1,19 @@
+const ora = require('ora');
 const { scan } = require('../../src/functions');
 
-describe('scan', () => {
-  it('succeeds', () => {
-    expect(true).toBeTruthy();
-  });
+const target = `${__dirname}/../project/`;
+
+const config = {
+  label: false,
+  backup: false,
+  frozen: false,
+  verbose: false,
+  pattern: '--caret'
+};
+
+describe('scan()', () => {
+  it('find expected vulnerabilities in listed depedencies', async () => {
+    const response = await scan(ora(), '', target, config);
+    expect(response).toEqual('detected 339 vulnerabilities');
+  }, 15000);
 });

ac70ea7e64d5c5191f5062d8472ee6df0c9b5cfa
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-09-24 16:16:36 +0200

    include file config in controller interpreter

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,12 +1,12 @@
 const { getCommand } = require('./commands');
 const { getFunctions } = require('./functions');
 
-const interpret = inputs => {
+const interpret = (inputs, fileConfig) => {
   const command = getCommand(inputs);
   if (command) return { command };
 
-  const { hint, functions, target } = getFunctions(inputs);
-  return { hint, functions, target };
+  const { hint, functions, target, config } = getFunctions(inputs, fileConfig);
+  return { hint, functions, target, config };
 };
 
 module.exports = {

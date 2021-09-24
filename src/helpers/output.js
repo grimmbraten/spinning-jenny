886e67a5c8e1de67ec350f837a42db003ca8dea6
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-09-24 18:50:01 +0200

    return message from loader output helper

--- src/helpers/output.js
+++ src/helpers/output.js
@@ -1,5 +1,7 @@
-const loader = (verbose, spinner, action, message, step, hint) =>
+const loader = (verbose, spinner, action, message, step, hint) => {
   verbose && spinner[action](step + message + hint);
+  return message;
+};
 
 const stepLabel = ({ label, steps, getStep }) => {
   if (!label) return '';

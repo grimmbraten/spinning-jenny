f39add97081848b95c6914f5677423e3f2ae133e
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-09-25 10:13:27 +0200

    add test constant file

---
+++ tests/constants/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  errorDir: `${__dirname}/../project/error/`,
+  secureDir: `${__dirname}/../project/secure/`,
+  config: {
+    label: false,
+    backup: false,
+    frozen: false,
+    verbose: false,
+    pattern: '--caret'
+  }
+};

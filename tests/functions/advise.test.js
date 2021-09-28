4ae574b6618bb4a2f9008d6728e05631639298b7
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-09-28 19:18:37 +0200

    cleanup and improve advise function tests

--- tests/functions/advise.test.js
+++ tests/functions/advise.test.js
@@ -1,15 +1,30 @@
+jest.mock('../../src/common/audit');
+jest.mock('../../src/helpers/data');
+const { audit } = require('../../src/common/audit');
+const { parseJson } = require('../../src/helpers/data');
+
 const { advise } = require('../../src/functions');
-const { errorDir, secureDir, config } = require('../constants');
+const { config, mockedAuditAdvisory } = require('../constants');
+
+const run = async () => await advise(undefined, undefined, undefined, config);
 
 describe('advise()', () => {
-  it('skips advise if no patches are available', async () => {
-    const response = await advise('', '', secureDir, config);
-    expect(response).toEqual('skipped advise');
+  it('fails if audit scan is unsuccessful', async () => {
+    audit.mockImplementationOnce(() => [false]);
+    expect(await run()).toEqual('scan failed');
+  });
+
+  it('skips if no patches are available', async () => {
+    audit.mockImplementationOnce(() => [true]);
+    parseJson.mockImplementationOnce(() => [{}]);
+
+    expect(await run()).toEqual('skipped advise');
   });
 
   it('succeeds if one or more patches are available', async () => {
-    const response = await advise('', '', errorDir, config);
-    expect(response).toContain('located');
-    expect(response).toContain('3');
+    audit.mockImplementationOnce(() => [true]);
+    parseJson.mockImplementationOnce(() => [mockedAuditAdvisory]);
+
+    expect(await run()).toContain('located 1 advisory');
   });
 });

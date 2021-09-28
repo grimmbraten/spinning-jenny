2dea625ce43be315f30b4778872adaad2b183f29
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-09-28 19:17:56 +0200

    remove success check in audit common

--- src/common/audit.js
+++ src/common/audit.js
@@ -5,8 +5,6 @@ const audit = async (spinner, hint, target, verbose, step) => {
   loader(verbose, spinner, 'start', 'scanning package.json', step, hint);
 
   const [success, response] = await execute(`yarn --cwd ${target} audit --json`);
-  if (!success) loader(verbose, spinner, 'fail', 'scan failed', step, hint);
-
   return [success, response];
 };
 

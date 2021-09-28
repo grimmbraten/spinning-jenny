1d0d3ebdeffa05f3e42b73bd7546c975426dae35
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-09-28 19:18:07 +0200

    add mocked audit advisory to constants

--- tests/constants/index.js
+++ tests/constants/index.js
@@ -7,5 +7,18 @@ module.exports = {
     frozen: false,
     verbose: false,
     pattern: '--caret'
+  },
+  mockedAuditAdvisory: {
+    type: 'auditAdvisory',
+    data: {
+      advisory: {
+        title: 'injection vulnerability',
+        module_name: 'mockery',
+        vulnerable_versions: '2.4.8',
+        patched_versions: '2.4.9',
+        severity: 'high',
+        url: 'https://www.npmjs.com/'
+      }
+    }
   }
 };

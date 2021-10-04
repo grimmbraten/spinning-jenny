f8dd238c535e19a7bfca134ab0389b6aff21a41b
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-10-04 14:31:55 +0200

    apply critical level flag to audit function

--- src/common/audit.js
+++ src/common/audit.js
@@ -3,9 +3,7 @@ const { loader } = require('../helpers');
 
 const audit = async (spinner, hint, target, verbose, step) => {
   loader(verbose, spinner, 'start', 'scanning package.json', step, hint);
-
-  const [success, response] = await execute(`yarn --cwd ${target} audit --json`);
-  return [success, response];
+  return await execute(`yarn --cwd ${target} audit --json --level critical`);
 };
 
 module.exports = {

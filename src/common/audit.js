148c3c1ca8ee311083ff6d03056dcf07ad47d2b5
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-10-04 15:27:17 +0200

    add scope parameter to audit function

--- src/common/audit.js
+++ src/common/audit.js
@@ -1,9 +1,9 @@
 const { execute } = require('./shell');
 const { loader } = require('../helpers');
 
-const audit = async (spinner, hint, target, verbose, step) => {
+const audit = async (spinner, hint, target, verbose, step, scope = '') => {
   loader(verbose, spinner, 'start', 'scanning package.json', step, hint);
-  return await execute(`yarn --cwd ${target} audit --json --level critical`);
+  return await execute(`yarn --cwd ${target} audit --json ${scope}`);
 };
 
 module.exports = {

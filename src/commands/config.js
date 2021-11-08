b175f06e788c2fc97aec21ae3959ba3d9d96b2a9
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2021-11-08 17:35:17 +0100

    fix manage config flow

--- src/commands/config.js
+++ src/commands/config.js
@@ -29,7 +29,7 @@ const manage = async (spinner, inputs) => {
   const config = await load();
 
   if (!inputs) return config;
-  inputs = inputs.slice(2);
+  inputs = inputs.slice(1);
 
   delete config.steps;
   delete config.getStep;
@@ -40,28 +40,28 @@ const manage = async (spinner, inputs) => {
 
     if (properties.verbose === input) {
       const value = parseBoolean(inputs[index + 1]);
-      if (value === undefined) return spinner.fail(`verbose can only be: ${trueFalse}`);
+      if (value === undefined) return spinner.fail(`verbose can only be ${trueFalse}`);
 
       config.verbose = value;
-      spinner.succeed('verbose: ' + colorProperty(value));
+      spinner.succeed('changed verbose to ' + colorProperty(value));
     } else if (properties.frozen === input) {
       const value = parseBoolean(inputs[index + 1]);
-      if (value === undefined) return spinner.fail(`frozen can only be: ${trueFalse}`);
+      if (value === undefined) return spinner.fail(`frozen can only be ${trueFalse}`);
 
       config.frozen = value;
-      spinner.succeed('frozen: ' + colorProperty(value));
+      spinner.succeed('changed frozen to ' + colorProperty(value));
     } else if (properties.backup === input) {
       const value = parseBoolean(inputs[index + 1]);
-      if (value === undefined) return spinner.fail(`backup can only be: ${trueFalse}`);
+      if (value === undefined) return spinner.fail(`backup can only be ${trueFalse}`);
 
       config.backup = value;
-      spinner.succeed('backup: ' + colorProperty(value));
+      spinner.succeed('changed backup to ' + colorProperty(value));
     } else if (properties.label === input) {
       const value = parseBoolean(inputs[index + 1]);
-      if (value === undefined) return spinner.fail(`label can only be: ${trueFalse}`);
+      if (value === undefined) return spinner.fail(`label can only be ${trueFalse}`);
 
       config.label = value;
-      spinner.succeed('label: ' + colorProperty(value));
+      spinner.succeed('changed label to ' + colorProperty(value));
     } else if (properties.pattern === input) {
       let value = inputs[index + 1];
       if (!value) return spinner.fail('please pass a value');
@@ -70,19 +70,14 @@ const manage = async (spinner, inputs) => {
 
       if (!['exact', 'caret', 'tilde'].includes(value))
         return spinner.fail(
-          `pattern can only be: ${chalk.gray('--exact')} / ${chalk.gray('--tilde')} / ${chalk.gray(
+          `pattern can only be ${chalk.gray('--exact')}, ${chalk.gray('--tilde')}, or ${chalk.gray(
             '--caret'
           )}`
         );
 
       config.pattern = value;
-      spinner.succeed('pattern: ', colorProperty(value));
-    } else return spinner.fail(`${input} is not a valid configuration property`);
-  });
-
-  console.log();
-  Object.keys(config).forEach(key => {
-    console.log(`${key}: ` + colorProperty(config[key]));
+      spinner.succeed('changed pattern to ', colorProperty(value));
+    } else return spinner.fail(`${input} is not a config property`);
   });
 
   await edit(config);
--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 
-const trueFalse = `${chalk.green('true')} / ${chalk.red('false')}`;
+const trueFalse = `${chalk.green('true')} or ${chalk.red('false')}`;
 
 module.exports = {
   trueFalse

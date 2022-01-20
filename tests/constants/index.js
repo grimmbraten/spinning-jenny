d3a3e64096dc2b4cdd3160570fd4d42428e602b2
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2022-01-20 15:03:36 +0100

    update mocked audit advisory object

--- tests/constants/index.js
+++ tests/constants/index.js
@@ -23,10 +23,17 @@ module.exports = {
   },
   mockedAuditAdvisory: {
     title: 'injection vulnerability',
+    version: '2.4.8',
     module: 'mockery',
-    versions: '2.4.8',
-    patched: '2.4.9',
+    vulnerableVersions: '<=2.4.8',
+    patchedVersions: '>=3.0.2',
     severity: 'high',
-    url: 'https://www.npmjs.com/'
+    url: 'https://github.com/advisories/MOCK-whgm-m0ck-g3j9',
+    recommendation: 'upgrade to version 3.0.2 or later',
+    created: 'Thu, 18 Nov 2021 16:00:48 GMT',
+    updated: 'Thu, 26 Aug 2021 14:50:46 GMT',
+    time: 1629989446000,
+    foundBy: undefined,
+    references: '- https://github.com/advisories/MOCK-whgm-m0ck-g3j9'
   }
 };

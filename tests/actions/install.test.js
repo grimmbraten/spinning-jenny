0d7a5fe46c4d1fcc9fda3b304b6982fb99793497
Erik Grimmbråten <79856585+grimmbraten@users.noreply.github.com>
2022-06-05 19:11:38 +0200

    cleanup and resolve install test issues after refactor

--- tests/actions/install.test.js
+++ tests/actions/install.test.js
@@ -1,28 +1,29 @@
-jest.mock('../../src/common');
-jest.mock('../../src/helpers/output');
-
-const { config } = require('../constants');
-const { execute } = require('../../src/common');
+const { config } = require('../mocks');
 const { install } = require('../../src/actions');
+
+const mockShell = jest.fn(() => [true, '']);
+jest.mock('../../src/services/shelljs', () => ({
+  shell: () => mockShell()
+}));
 
 const run = async (mockConfig = config) => await install(undefined, undefined, mockConfig);
 
-describe('install()', () => {
+describe('[actions] install', () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it('skips if config frozen is true', async () => {
+  it('exists with [StatusCode:1] if config frozen is true', async () => {
     expect(await run({ ...config, frozen: true })).toEqual(1);
   });
 
-  it('fails if installation encountered an error', async () => {
-    execute.mockImplementationOnce(() => [false]);
+  it('exists with [StatusCode:2] if installation encountered an error', async () => {
+    mockShell.mockImplementationOnce(() => [false, ['mocked', 'response']]);
     expect(await run()).toEqual(2);
   });
 
-  it('succeeds if installation completes without any issues', async () => {
-    execute.mockImplementationOnce(() => [true]);
+  it('exists with [StatusCode:0] if installation completes without any issues', async () => {
+    mockShell.mockImplementationOnce(() => [true]);
     expect(await run()).toEqual(0);
   });
 });
